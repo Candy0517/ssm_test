89241d9f818cbf926108870a7bb47a42f03fbb92
duanxvsheng <duanxvsheng@126.com>
2019-06-16 23:57:46 +0800

    dao层上传

---
+++ ssm_test/ssm_dao/src/test/java/cn/itcast/test/DaoTest.java
@@ -0,0 +1,19 @@
+package cn.itcast.test;
+
+import cn.itcast.dao.ItemsDao;
+import cn.itcast.domain.Items;
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class DaoTest {
+
+    @Test
+    public void test1(){
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        ItemsDao itemsDao = context.getBean(ItemsDao.class);
+        Items items = itemsDao.findById(1);
+        System.out.println(items);
+
+    }
+}

715596df17b6284b7014cd85f0bb751b300a42d1
duanxvsheng <duanxvsheng@126.com>
2019-06-17 13:37:27 +0800

    web上传

--- ssm_test/ssm_web/src/main/java/cn/itcast/controller/ItemsController.java
+++ ssm_test/ssm_web/src/main/java/cn/itcast/controller/ItemsController.java
@@ -9,7 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/itmes")
+@RequestMapping("/items")
 public class ItemsController {
 
 
